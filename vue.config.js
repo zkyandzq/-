f7f37845c813ca205cfe1e4b3538e3c21b1e6835
zkyandzq <3334526998@qq.com>
2023-07-04 16:03:13 +0800

    feat: 内容的变化

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,24 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  productionSourceMap: "./",
+  outputDir: "dist/msg",
+  assetsDir: "assets",
+  devServer: {
+    port: 6767,
+    host: "0.0.0.0",
+    https: false,
+    open: true,
+    proxy: {
+      "/api": {
+        changeOrigin: true,
+        secure: false,
+        target: "http://ihrm.itheima.net/",
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+  }
 })
